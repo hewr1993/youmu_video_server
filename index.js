0c8b3c5d7c53758b07175d761cfae5c6ff2e0e5c
hewr1993 <hewr2010@gmail.com>
2014-10-14 14:50:53 +0800

    demo for video server

---
+++ index.js
@@ -0,0 +1,39 @@
+/*
+ * Author:	Wayne Ho
+ * Mail:	hewr2010@gmail.com 
+ * Time:	Tue 14 Oct 2014 02:39:09 PM CST
+ * Purpose:	Simple Server for Streaming Video (from StackOverflow)
+ */
+
+var http = require('http');
+var fs = require('fs');
+var util = require('util');
+
+var host = "127.0.0.1", port = "8888";
+var url = "http://" + host + ":" + port + "/";
+
+http.createServer(function(req, res) {
+	var path = "videos/你的名字我的姓氏.mp4";
+	var stat = fs.statSync(path);
+	if (req.headers['range']) {
+		var ranges = req.headers.range.replace(/bytes=/, "").split("-");
+		var start = parseInt(ranges[0], 10);
+		var end = ranges[1] ? parseInt(ranges[1], 10) : stat.size - 1;
+		var chunksize = (end - start) + 1;
+		console.log('RANGE: ' + start + ' - ' + end + ' = ' + chunksize);
+		
+		var file = fs.createReadStream(path, {start: start, end: end});
+		res.writeHead(206, {'Content-Range': 'bytes ' + start + '-' + end + '/' + stat.size, 
+							'Content-Length': chunksize, 
+							'Accept-Ranges': 'bytes', 
+							'Content-Type': 'video/mp4' });
+		file.pipe(res);
+	} else {
+		console.log('ALL: ' + stat.size);
+		res.writeHead(200, {'Content-Length': stat.size, 
+							'Content-Type': 'video/mp4' });
+		fs.createReadStream(path).pipe(res);
+	}
+}).listen(port, host);
+console.log("server running at " + url);
+
--- readme.md
+++ readme.md
@@ -1,6 +1,20 @@
 # YouMu Video Server
 - - -
 
-* NodeJS based brute-force server
-* Protocols
-* P2P
+## 基于NodeJS暴力视频端
+* 目前先只支持mp4格式，返回同一个视频
+* 视频素材存放在/videos/
+
+## 用法
+* 安装nodejs
+* 修改*index.js*中端口参数（默认8888）
+
+	node index.js
+
+* 访问[本机8888端口](http://localhost:8888)即可
+
+## TODO List
+* 使用package.json规范参数
+* 用id指定视频
+* 分段传输视频数据
+
