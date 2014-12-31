a9f023e8d4d1010da40bced2cf747eb4c66c8773
hewr1993 <hewr2010@gmail.com>
2014-12-31 15:58:56 +0800

    rtmp

--- rtmp.sh
+++ rtmp.sh
@@ -1,1 +1,1 @@
-ffmpeg -re -i "/Users/hewr1993/Movies/2.mp4" -vcodec libx264 -vprofile baseline -acodec aac -ar 44100 -strict -2 -ac 1 -f flv -s 1280x720 -q 10 rtmp://166.111.206.70:1935/myapp/test1
+ffmpeg -re -i "/Users/hewr1993/Movies/2.mp4" -vcodec libx264 -vprofile baseline -acodec aac -ar 44100 -strict -2 -ac 1 -f flv -s 1280x720 -q 10 rtmp://166.111.206.70:1935/myapp/6
